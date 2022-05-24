fcac9b7bf68926a63f0f471359eba4fae806cceb
이규형 <rbgud2380@naver.com>
2022-05-24 17:55:59 +0900

    add check password with js

--- src/main/resources/static/js/default.js
+++ src/main/resources/static/js/default.js
@@ -22,17 +22,43 @@ $(function() {
         }
     }
     function chkJoinForm() {
-        const name = $('input[name=join_name]').val();
-        const jemail = $('input[name=join_email]').val();
-        const jpwd = $('input[name=join_pwd]').val();
-        const jpwd2 = $('input[name=join_pwd2]').val();
+        const name = $('input[name=join_name]');
+        const email = $('input[name=join_email]');
+        const pwd = $('input[name=join_pwd]');
+        const pwd2 = $('input[name=join_pwd2]');
+
+        if(name.val() == '' || name.val() == undefined) {
+            alert('이름을 입력하세요.');
+            name.focus();
+            return;
+        }
+        if(email.val() == '' || email.val() == undefined) {
+            alert('이메일을 입력하세요.');
+            email.focus();
+            return;
+        }
+        if(pwd.val() == '' || pwd.val() == undefined) {
+            alert('비밀번호를 입력하세요.');
+            pwd.focus();
+            return;
+        }
+        if(pwd2.val() == '' || pwd2.val() == undefined) {
+            alert('비밀번호확인 입력하세요.');
+            pwd2.focus();
+            return;
+        }
+        if(pwd.val() != pwd2.val()){
+            alert('비밀번호를 다시 한번 확인해주세요.');
+            pwd.focus();
+            return;
+        }
     }
 
     $('.btn_join').click(() => {
         chkJoinForm();
 
         console.log('클릭');
-        console.log("form value : " + $('.form_login').serialize());
+        console.log("form value : " + $('.form_join').serialize());
     })
 
 })

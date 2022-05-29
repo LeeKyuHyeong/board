cebff7c76f7759629f2f617197afb810b814de01
이규형 <rbgud2380@naver.com>
2022-05-29 15:18:56 +0900

    add player.jpg

--- src/main/java/com/board/controller/UserController.java
+++ src/main/java/com/board/controller/UserController.java
@@ -22,78 +22,74 @@ import lombok.RequiredArgsConstructor;
 @SessionAttributes("user")
 public class UserController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
-    
-    
-    //private final SessionManager sessionManager;
+
+    // private final SessionManager sessionManager;
 
     @Autowired
     private final LoginService loginService;
 
     @RequestMapping("/login")
     public String loginId(
-        @RequestParam(value = "login_id") String id,
-        @RequestParam(value = "login_pwd") String pwd, 
-        HttpServletResponse response, 
-        HttpServletRequest request,
-        Model model) {
-                        
+            @RequestParam(value = "login_id") String id,
+            @RequestParam(value = "login_pwd") String pwd,
+            HttpServletResponse response,
+            HttpServletRequest request,
+            Model model) {
+
         log.info("userInfo! : " + id + ", " + pwd);
         UserEntity findUser = loginService.getUser(id, pwd);
-        
-        if(findUser != null && findUser.getUser_loginpwd().equals(pwd)) {
+
+        if (findUser != null && findUser.getUser_loginpwd().equals(pwd)) {
             model.addAttribute("user", findUser);
             return "forward:viewteamList";
-        } else {            
+        } else {
             return "redirect:/";
         }
 
         // if(!loginService.login(userEntity)) {
-        //     return "login";
+        // return "login";
         // }
 
-        //sessionManager.createSession(userEntity, response);
+        // sessionManager.createSession(userEntity, response);
 
         // return "redirect:/";
     }
 
-
-
-
     // @PostMapping(value = "/login")
     // public String login(
-    //         @RequestParam(value = "login_id", required = true) String loginId,
-    //         @RequestParam(value = "login_pwd", required = true) String loginPwd,
-    //         Model model,
-    //         HttpServletRequest request,
-    //         HttpServletResponse response,
-    //         HttpSession session) throws Exception {
-            
-    //     String msg = "";
-        
-    //     try {            
-    //         log.info("loginId : " + loginId);
-    //         log.info("loginPwd : " + loginPwd);
+    // @RequestParam(value = "login_id", required = true) String loginId,
+    // @RequestParam(value = "login_pwd", required = true) String loginPwd,
+    // Model model,
+    // HttpServletRequest request,
+    // HttpServletResponse response,
+    // HttpSession session) throws Exception {
 
-    //         // 패스워드 암호화 SHA512Hash.getDigest("1234")
-    //         // param.put("password", SHA512Hash.getDigest(loginPwd));            
+    // String msg = "";
 
-    //         int cnt = userRepository.findByIdAndPassword(loginId, loginPwd);
-    //         log.info(String.valueOf(cnt));
+    // try {
+    // log.info("loginId : " + loginId);
+    // log.info("loginPwd : " + loginPwd);
 
-    //         if(cnt > 0) {
-    //             model.addAttribute("msg", "success");
-    //             msg = "success";
-    //         } else {
-    //             model.addAttribute("msg", "failed");
-    //             msg = "failed";
-    //         }            
+    // // 패스워드 암호화 SHA512Hash.getDigest("1234")
+    // // param.put("password", SHA512Hash.getDigest(loginPwd));
 
-    //     } catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    //     log.info(msg);
-    //     return msg;
-    //     // return "thymeleaf/main2";
+    // int cnt = userRepository.findByIdAndPassword(loginId, loginPwd);
+    // log.info(String.valueOf(cnt));
+
+    // if(cnt > 0) {
+    // model.addAttribute("msg", "success");
+    // msg = "success";
+    // } else {
+    // model.addAttribute("msg", "failed");
+    // msg = "failed";
+    // }
+
+    // } catch (Exception e) {
+    // e.printStackTrace();
+    // }
+    // log.info(msg);
+    // return msg;
+    // // return "thymeleaf/main2";
     // }
 
 }
